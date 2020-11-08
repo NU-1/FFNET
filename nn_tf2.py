60481bc63e3484e31e7d1eeb0bae1e0488279860
NU-1 <algorithm0110+@gmail.com>
2020-11-08 00:55:18 -0600

    Transforming nn.py to TF2 w/ Keras

--- nn_tf2.py
+++ nn_tf2.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 
 #TF2 adaptation of nn.py to maintain compatibility
-
 #loss type should be "mse" instead of "mean_square"
 
 #https://www.tensorflow.org/api_docs/python/tf/keras/Model
@@ -17,22 +16,24 @@ class NeuralNet(tf.keras.Model):
         self.hidden_layers = []
         for i in layers[:-1]:
             self.hidden_layers.append(tf.keras.layers.Dense(
-                layers[i].num_output, activation=layers[i].activation_type, kernel_initializer='RandomNormal'))
+                layers[i].num_output, activation=layers[i].activation_type,
+                kernel_initializer='RandomNormal'))
 
         self.output_layer = tf.keras.layers.Dense(
-            layers[-1].num_output , activation=layers[-1].activation_type, kernel_initializer='RandomNormal')
-
+            layers[-1].num_output , activation=layers[-1].activation_type,
+            kernel_initializer='RandomNormal')
         
         if opt_type=='SGD':
-        	opt = tf.keras.optimizers.SGD(learning_rate=learning_rate_)
-		else: 
-			opt = tf.keras.optimizers.RMSprop(learning_rate=learning_rate_)
+            opt = tf.keras.optimizers.SGD(learning_rate=learning_rate_)
+	else: 
+	    opt = tf.keras.optimizers.RMSprop(learning_rate=learning_rate_)
 
         self.compile(optimizer=opt, loss=loss_type)
     
     
     def forward(self, data):
     	return self.call(data)
+
 
     @tf.function
     def call(self, data):
@@ -48,24 +49,25 @@ class NeuralNet(tf.keras.Model):
 
 
     def recover(self,path,file):
-		try:
-			self.load_weights(path + file)
-		except:
-			print('Error when restoring weights!')
+        try:
+            self.load_weights(path + file)
+        except:
+            print('Error when restoring weights!')
 
 
-	def saving(self,path,file):
-		try:
-			self.save_weights(path + file)
-		except:
-			print('Error when saving weights!')
+    def saving(self,path,file):
+        try:
+            self.save_weights(path + file)
+        except:
+            print('Error when saving weights!')
 
 
-	def to_TFLite(self):
-		#convert 
-		converter = tf.lite.TFLiteConverter.from_keras_model(self)
-		tflite_model = converter.convert()
+    def to_TFLite(self):
+        #convert 
+        converter = tf.lite.TFLiteConverter.from_keras_model(self)
+        tflite_model = converter.convert()
 
-		# Save the model.
-		with open('model.tflite', 'wb') as f:
-  			f.write(tflite_model)
+        # Save the model.
+        with open('model.tflite', 'wb') as f:
+            f.write(tflite_model)
+
