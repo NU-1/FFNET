87c1279bb1444ef019cd136904d5766d54a51c17
NU-1 <algorithm0110+@gmail.com>
2020-11-29 00:11:53 -0600

    Send and receive floats to arduino via serial

--- Deploy_Host/py_to_arduino.py
+++ Deploy_Host/py_to_arduino.py
@@ -9,35 +9,15 @@ time.sleep(2) # allow some time for the Arduino to completely reset
 while True:
     send_size = 0
     
-    ###################################################################
-    # Send a list
-    ###################################################################
-    list_ = [1, 3]
-    list_size = link.tx_obj(list_)
-    send_size += list_size
-    
-    ###################################################################
-    # Send a string
-    ###################################################################
-    str_ = 'hello'
-    str_size = link.tx_obj(str_, send_size) - send_size
-    send_size += str_size
-    
-    ###################################################################
-    # Send a float
-    ###################################################################
+    #send a float
     float_ = 5.234
     float_size = link.tx_obj(float_, send_size) - send_size
     send_size += float_size
     
-    ###################################################################
-    # Transmit all the data to send in a single packet
-    ###################################################################
+    #transmit float
     link.send(send_size)
     
-    ###################################################################
-    # Wait for a response and report any errors while receiving packets
-    ###################################################################
+    #wait for response
     while not link.available():
         if link.status < 0:
             if link.status == txfer.CRC_ERROR:
@@ -49,32 +29,17 @@ while True:
             else:
                 print('ERROR: {}'.format(link.status))
     
-    ###################################################################
-    # Parse response list
-    ###################################################################
-    rec_list_  = link.rx_obj(obj_type=type(list_),
-                             obj_byte_size=list_size,
-                             list_format='i')
     
-    ###################################################################
-    # Parse response string
-    ###################################################################
-    rec_str_   = link.rx_obj(obj_type=type(str_),
-                             obj_byte_size=str_size,
-                             start_pos=list_size)
     
-    ###################################################################
-    # Parse response float
-    ###################################################################
+    #parse float
     rec_float_ = link.rx_obj(obj_type=type(float_),
                              obj_byte_size=float_size,
-                             start_pos=(list_size + str_size))
+                             start_pos=(0))
     
-    ###################################################################
-    # Display the received data
-    ###################################################################
-    print('SENT: {} {} {}'.format(list_, str_, float_))
-    print('RCVD: {} {} {}'.format(rec_list_, rec_str_, rec_float_))
+
+    #display data
+    print('SENT: {}'.format(float_))
+    print('RCVD: {} '.format(rec_float_))
     print(' ')
 
 try:
